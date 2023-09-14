8eda2a896d53ebba087d5ab081e792432446cd55
Guterresthomas <guterresmikael@gmail.com>
2023-09-14 19:34:46 -0300

    enum

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        
-        self.width > other.width && self.height > other.height
-
-    }
-}
-
-fn main() {
-    
-    let rect1 = Rectangle {
-        width: 30,
-        height:50,
-    };
-
-    let rect2 = Rectangle {
-        width: 10,
-        height :40,
-    };
-
-    let rect3 = Rectangle {
-        width: 60, 
-        height: 45,
-    };
-
-    println!("rect1 is {:?}", rect1);
-
-        println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
-        println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
-    
+enum IpAddrKind {
+    v4,
+    v6,
 }
 
 
 
+fn main () {
+    let four = IpAddrKind::v4;
+    let six: IpAddrKind = IpAddrKind::v6;
+
+    route(IpAddrKind::v4);
+    route(IpAddrKind::v6);
+
+}
+
+fn route(ip_kind: IpAddrKind) {}
